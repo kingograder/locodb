f12e19a97040e7c1f2ca1213ec4075897bd59040
kingograder <aleksey.kaplinsky@proton.me>
2026-09-18 01:22:22 +0300

    удалил ненужные файлы

--- app/db/db.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-engine = create_engine("sqlite:///./data/database.sqlite", echo=True)
-session_factory = sessionmaker(engine, expire_on_commit=False)
