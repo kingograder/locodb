ef367be079d9ed9d0ab346760d04fa6e3cdc8676
Grand_Maket_1 <alexey>
2026-09-23 13:32:39 +0300

    улучшен ui_compile

--- ui_compile.py
+++ ui_compile.py
@@ -3,19 +3,16 @@ import subprocess
 
 INPUT_DIR = Path("uis")
 OUTPUT_DIR = Path("app/widgets")
+
+# Создаем целевую папку, если её нет
 OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
-# Использование rglob для рекурсивного поиска всех файлов .ui
+# Рекурсивный обход всех поддиректорий
 for file in INPUT_DIR.rglob("*.ui"):
-    # Вычисляем относительный путь файла по отношению к INPUT_DIR
-    relative_path = file.relative_to(INPUT_DIR)
+    # Формируем путь для .py файла прямо в корне OUTPUT_DIR (плоская структура)
+    out_file = OUTPUT_DIR / f"ui_{file.stem}.py"
 
-    # Формируем путь для выходного файла с сохранением подпапок
-    out_file = OUTPUT_DIR / relative_path.parent / f"ui_{file.stem}.py"
-
-    # Создаем поддиректории, если их еще нет
-    out_file.parent.mkdir(parents=True, exist_ok=True)
-
+    # Запускаем компиляцию
     subprocess.run(
         ["pyside6-uic", str(file), "-o", str(out_file)],
         check=True,
