83ba79d8bfb7e44567357fc59c796242824769b8
Grand_Maket_1 <alexey>
2026-09-23 12:32:53 +0300

    рекурсивно обходит дирректории внтурни выбранной

--- ui_compile.py
+++ ui_compile.py
@@ -5,8 +5,17 @@ INPUT_DIR = Path("uis")
 OUTPUT_DIR = Path("app/widgets")
 OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
-for file in INPUT_DIR.glob("*.ui"):
-    out_file = OUTPUT_DIR / f"ui_{file.stem}.py"
+# Использование rglob для рекурсивного поиска всех файлов .ui
+for file in INPUT_DIR.rglob("*.ui"):
+    # Вычисляем относительный путь файла по отношению к INPUT_DIR
+    relative_path = file.relative_to(INPUT_DIR)
+
+    # Формируем путь для выходного файла с сохранением подпапок
+    out_file = OUTPUT_DIR / relative_path.parent / f"ui_{file.stem}.py"
+
+    # Создаем поддиректории, если их еще нет
+    out_file.parent.mkdir(parents=True, exist_ok=True)
+
     subprocess.run(
         ["pyside6-uic", str(file), "-o", str(out_file)],
         check=True,
